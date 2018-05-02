687446d618ca0668c3a00bdf2ea40c9f368d9f46
vdminkovi <33345456+vdminkovi@users.noreply.github.com>
2018-05-02 15:56:48 +0300

    task

---
+++ README.md
@@ -0,0 +1,5 @@
+•	HTML, CSS and JavaScript are all attempted 
+•	For it to look presentable - but does not have to be ‘pixel perfect’
+•	FAQ’s are rendered dynamically : FAQ’s are provided in JSON format. Ideally these would be AJAX’d in.
+•	Clean code
+•	Something that works after 30 minutes!
---
+++ script.js
@@ -0,0 +1,38 @@
+// custom script
+(function($) {
+	
+	$.fn.Accordion = function() {
+		
+	var accItem = $(".accordion .accordion-at");
+	var i;
+	
+	$(accItem).on("click", '.icon', function() {
+		if($(this).parents('.accordion-at').hasClass("active")){
+			$(this).parents('.accordion-at').removeClass("active");
+		} else {
+			$(this).parents('.accordion-at').addClass('active');
+			$(this).parents('.accordion-at').siblings().removeClass('active');
+		}
+
+	});
+}
+
+}(jQuery));
+
+$( document ).ready(function() {
+
+	$.getJSON( "faqs.json", function( data ) {
+		var items = [];
+		$.each( data.faqs, function( key, val ) {
+		items.push( "<dl class=\"accordion-at\"><dt><span>" + val.question + "<span><span class=\"icon\"></span></dt><dd><p>" + val.answer + "</p></dd></dl>" );
+		});
+	 
+		$( "<div/>", {
+		"class": "accordion",
+		html: items.join( "" )
+		}).appendTo( "body" );
+	}).done(function(){				
+		$(".accordion").Accordion();
+	});
+
+});
