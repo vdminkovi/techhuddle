41d2fcede0e2f16d2276c4bf8b699d659b672a2d
vdminkovi <33345456+vdminkovi@users.noreply.github.com>
2018-05-03 13:35:26 +0300

    fsafsa

--- script.js
+++ script.js
@@ -1,38 +1,29 @@
 // custom script
-(function($) {
-	
-	$.fn.Accordion = function() {
-		
-	var accItem = $(".accordion .accordion-at");
-	var i;
-	
-	$(accItem).on("click", '.icon', function() {
-		if($(this).parents('.accordion-at').hasClass("active")){
-			$(this).parents('.accordion-at').removeClass("active");
+(function($) {	
+	$.fn.Accordion = function() {		
+	var accItem = $('.accordion .accordion-at');
+	$(accItem).on('click', '.icon', function() {
+		if($(this).parents('.accordion-at').hasClass('active')){
+			$(this).parents('.accordion-at').removeClass('active');
 		} else {
 			$(this).parents('.accordion-at').addClass('active');
 			$(this).parents('.accordion-at').siblings().removeClass('active');
 		}
-
 	});
 }
 
 }(jQuery));
-
 $( document ).ready(function() {
-
-	$.getJSON( "faqs.json", function( data ) {
+	$.getJSON( 'faqs.json', function( data ) {
 		var items = [];
 		$.each( data.faqs, function( key, val ) {
-		items.push( "<dl class=\"accordion-at\"><dt><span>" + val.question + "<span><span class=\"icon\"></span></dt><dd><p>" + val.answer + "</p></dd></dl>" );
-		});
-	 
-		$( "<div/>", {
-		"class": "accordion",
+		items.push( '<dl class=\'accordion-at\'><dt><span>' + val.question + '<span><span class=\'icon\'></span></dt><dd><p>' + val.answer + '</p></dd></dl>' );
+		});	 
+		$( '<div/>', {
+		'class': 'accordion',
 		html: items.join( "" )
-		}).appendTo( "body" );
+		}).appendTo( 'body');
 	}).done(function(){				
-		$(".accordion").Accordion();
+		$('.accordion').Accordion();
 	});
-
 });
